8213de929814e4ac06be9b777809f4e06956953f
Your Name <you@example.com>
2026-05-12 21:44:10 +0630

    Delivery rider app upload

---
+++ android/app/src/main/kotlin/com/sixamtech/efood_multivendor_driver/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.myanfood.rider
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity : FlutterActivity() {
+
+    private val CHANNEL = "com.sixamtech/app_retain"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+            .setMethodCallHandler { call, result ->
+                if (call.method == "sendToBackground") {
+                    moveTaskToBack(true)
+                    result.success(null)
+                } else {
+                    result.notImplemented()
+                }
+            }
+    }
+}
